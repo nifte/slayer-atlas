1ca11f37652427ba207678f27191b8727dc0d4e0
nifte <9247037+nifte@users.noreply.github.com>
2026-08-31 13:43:09 -0700

    adjust wiki button styling

--- src/main/java/com/slayeratlas/ui/ItemWikiMenu.java
+++ src/main/java/com/slayeratlas/ui/ItemWikiMenu.java
@@ -1,13 +1,10 @@
 package com.slayeratlas.ui;
 
 import com.slayeratlas.data.GearItem;
-import java.awt.Color;
-import java.awt.Cursor;
 import java.util.function.Consumer;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import javax.swing.SwingConstants;
-import net.runelite.client.ui.ColorScheme;
+import javax.swing.border.EmptyBorder;
 import net.runelite.client.util.LinkBrowser;
 
 public final class ItemWikiMenu
@@ -34,16 +31,9 @@ public final class ItemWikiMenu
 		}
 		JPopupMenu menu = new JPopupMenu();
 		menu.setName("item-wiki-menu");
-		menu.setBackground(ColorScheme.DARKER_GRAY_COLOR);
-		JMenuItem wiki = new JMenuItem(PanelCopy.OPEN_WIKI, ExternalLinkIcon.icon());
+		menu.setBorder(new EmptyBorder(5, 5, 5, 5));
+		JMenuItem wiki = new JMenuItem(PanelCopy.OPEN_WIKI);
 		wiki.setName("item-wiki");
-		wiki.setFont(PanelFonts.bodyBold());
-		wiki.setForeground(Color.WHITE);
-		wiki.setBackground(ColorScheme.DARKER_GRAY_COLOR);
-		wiki.setOpaque(true);
-		wiki.setHorizontalTextPosition(SwingConstants.LEFT);
-		wiki.setIconTextGap(6);
-		wiki.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		wiki.addActionListener(event -> openUrl.accept(url));
 		menu.add(wiki);
 		return menu;
--- src/test/java/com/slayeratlas/ui/ItemWikiMenuTest.java
+++ src/test/java/com/slayeratlas/ui/ItemWikiMenuTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNull;
 
 import com.slayeratlas.ComponentLookup;
 import com.slayeratlas.data.GearItem;
+import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JMenuItem;
@@ -24,6 +25,18 @@ public class ItemWikiMenuTest
 		assertEquals(PanelCopy.OPEN_WIKI, wiki.getText());
 		wiki.doClick();
 		assertEquals(List.of(WikiItemUrl.fromName("Shark")), opened);
+	}
+
+	@Test
+	public void showsWikiAsPlainTextWithoutAnIcon()
+	{
+		JPopupMenu menu = ItemWikiMenu.forItem(GearItem.named("Shark"), url ->
+		{
+		});
+		JMenuItem wiki = (JMenuItem) ComponentLookup.named(menu, "item-wiki");
+		assertNull(wiki.getIcon());
+		assertEquals(PanelCopy.OPEN_WIKI, wiki.getText());
+		assertEquals(new Insets(5, 5, 5, 5), menu.getInsets());
 	}
 
 	@Test
